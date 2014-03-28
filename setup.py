0fef9d6b97148a2ed455bdbee2a7690d5020fe19
Dmitry Orlov <dorlov@undev.ru>
2014-03-28 23:28:46 +0400

    [fix] set version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='serial2tcp',
-    version='1.0.0',
+    version='0.5',
     url='http://www.cs.earlham.edu/~charliep/ecoi/serial/pyserial-2.2/examples/tcp_serial_redirect.py',
     download_url='https://github.com/mosquito/serialtcp/archive/master.zip',
     description='Convert serial device to tcp socket.',
